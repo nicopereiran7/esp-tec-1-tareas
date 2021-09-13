9ad3f3f5c4031c3fc8e4526256c41bfca710a67e
Nicolas <nicopereiran7n7n7@gmail.com>
2021-09-13 13:56:05 -0300

    actividad 3 parte 1 lista

---
+++ unidad-3/act-3-guia-javascript/1/main.js
@@ -0,0 +1,16 @@
+let num1 = document.getElementById("num1");
+let btn = document.getElementById("btn");
+let resultado = document.getElementById("resultado");
+
+btn.addEventListener("click", () => {
+  if (!num1.value || !num2.value) {
+    alert("Ingrese numeros");
+  } else {
+    let value_1 = parseInt(num1.value);
+    let value_2 = parseInt(num2.value);
+
+    resultado.innerHTML = getRandom(value_1, value_2);
+  }
+});
+
+const getRandom = (min, max) => Math.floor(Math.random() * (max - min)) + min;
